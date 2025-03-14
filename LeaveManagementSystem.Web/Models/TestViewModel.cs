4a2d7df57c8693268cec189f45f4a1f2759eb95f
Chloé Brunet-Bellemare <1757610@cstj.qc.ca>
2025-03-14 19:40:14 -0400

    first commit

---
+++ LeaveManagementSystem.Web/Models/TestViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LeaveManagementSystem.Web.Models;
+
+public class TestViewModel
+{   
+    public string? Name { get; set; }
+    public DateTime? DateOfBirth { get; set; }
+    
+
+}
