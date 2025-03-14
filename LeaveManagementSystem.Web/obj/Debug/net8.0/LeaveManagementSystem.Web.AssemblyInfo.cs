1b9cfd1aefa2ff6c893afe483c8b48579dcab284
Chloé Brunet-Bellemare <1757610@cstj.qc.ca>
2025-03-14 19:42:29 -0400

    test

--- LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
+++ LeaveManagementSystem.Web/obj/Debug/net8.0/LeaveManagementSystem.Web.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a2d7df57c8693268cec189f45f4a1f2759eb95f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LeaveManagementSystem.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
